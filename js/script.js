ae52f2b7d3b4fb30a326cb0bd25b74597696bdbe
Nathan Beck <natebeck18@gmail.com>
2023-11-13 22:57:07 -0500

    Junior year changes: - Update resume - Redo style - Update about me - Bootstrap

--- js/script.js
+++ js/script.js
@@ -1,42 +1,59 @@
-const typing = document.querySelector('.typing');
-const words = ["Student", "Back-end Developer", "Machine Learning Enthusiast"];
-let i = 0;
-let j = 0;
-let currentWord = '';
-let isDeleting = false;
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Code for smooth scroll animation
+  const navLinks = document.querySelectorAll("nav ul li a");
 
-function type() {
-  currentWord = words[i % words.length];
-  if (isDeleting) {
-    typing.textContent = currentWord.substring(0, j-1);
-    j--;
-    if (j == 0) {
-      isDeleting = false;
-      i++;
-      // Pause after deleting a word
-      setTimeout(type, 500);
-    } else {
-      setTimeout(type, 50);
-    }
-  } else {
-    typing.textContent = currentWord.substring(0, j+1);
-    j++;
-    if (j == currentWord.length) {
-      isDeleting = true;
-      // Pause after typing a word
-      if (i == words.length - 1) {
-        // Pause on the last word for 5 seconds
-        setTimeout(() => {
-          isDeleting = true; // set isDeleting to true to continue with backspace animation
-          type(); // continue with backspace animation
-        }, 5000);
-      } else {
-        setTimeout(type, 1000);
+  for (const link of navLinks) {
+    link.addEventListener("click", function (e) {
+      // Check if the link is not one of GitHub, Resume, or LinkedIn
+      if (
+        !this.href.includes("github.com") &&
+        !this.href.includes("linkedin.com") &&
+        !this.href.endsWith(".pdf")
+      ) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
       }
-    } else {
-      setTimeout(type, 50);
-    }
+    });
   }
-}
+});
 
-type();
+document.addEventListener("DOMContentLoaded", () => {
+  const professions = [
+    " student",
+    "n app developer",
+    " machine learning enthusiast",
+  ];
+  let professionIndex = 0;
+  let charIndex = 0;
+  let currentText = "";
+  let isDeleting = false;
+
+  const typewriterElement = document.querySelector("#profession");
+
+  function typeWriterEffect() {
+    if (professionIndex >= professions.length) {
+      professionIndex = 0;
+    }
+
+    if (!isDeleting && charIndex <= professions[professionIndex].length) {
+      currentText = professions[professionIndex].slice(0, ++charIndex);
+    } else if (isDeleting && charIndex > 0) {
+      currentText = professions[professionIndex].slice(0, --charIndex);
+    }
+
+    typewriterElement.textContent = currentText;
+
+    if (!isDeleting && charIndex === professions[professionIndex].length) {
+      setTimeout(() => (isDeleting = true), 2000); // Pause at end
+    } else if (isDeleting && charIndex === 0) {
+      isDeleting = false;
+      professionIndex++;
+    }
+
+    setTimeout(typeWriterEffect, isDeleting ? 50 : 100); // Speed of typing
+  }
+
+  typeWriterEffect();
+});
